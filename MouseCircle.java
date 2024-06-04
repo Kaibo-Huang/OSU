2cf06cc126b0284819152920d030a6ae14e22e5d
Kaibo Huang <99372604+KaiboHuang1@users.noreply.github.com>
2024-06-03 22:23:34 -0400

    Update MouseCircle.java

--- MouseCircle.java
+++ MouseCircle.java
@@ -1,7 +1,38 @@
 import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
 
 public class MouseCircle extends Rectangle {
+    public int radius = 200; // initial size of the circle
+    public static final int MAX_RADIUS = 200; // Maximum radius size
+    public static final int MIN_RADIUS = 60;  // Minimum radius size
 
+    // constructor creates circle at given location with given dimensions
+    public MouseCircle(int centerX, int centerY) {
+        super(centerX - 200 / 2, centerY - 200 / 2, 200, 200);
+    }
+
+    // draws the current location of the circle to the screen
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.fillOval(x, y, radius, radius);
+        
+        g.setColor(Color.white);
+        g.fillOval(x + 5, y + 5, radius - 10, radius - 10);
+    }
+
+    // method to set the radius and update the rectangle bounds
+    public void setRadius(int r) {
+        radius = r;
+        
+    }
+
+    // method to get the radius
+    public int getRadius() {
+        return radius;
+    }
+
+    // method to update the position based on the center
+    public void setPosition(int centerX, int centerY) {
+        this.x = centerX;
+        this.y = centerY;
+    }
 }

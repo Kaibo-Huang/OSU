f3e3cff89aefcdba0ce490a9fadfc3c35fcd49c5
DonTranne <131465716+DonTranne@users.noreply.github.com>
2024-06-05 22:19:44 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+//Don Tran and Kaibo Huang
+//June 11, 2024
+//This class will create a new GameFrame in order to create a window and start the game.
+
 class Main {
   public static void main(String[] args) {
 

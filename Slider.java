558cbf19204d3f22992afbbc93efb745dbe878b2
DonTranne <131465716+DonTranne@users.noreply.github.com>
2024-06-05 22:21:33 -0400

    Update Slider.java

--- Slider.java
+++ Slider.java
@@ -1,3 +1,7 @@
+//Don Tran and Kaibo Huang
+//June 11, 2024
+//This class creates a new slider object, which will require the user to hover over the shape while holding down their mouse or keyboard bindings. 
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
